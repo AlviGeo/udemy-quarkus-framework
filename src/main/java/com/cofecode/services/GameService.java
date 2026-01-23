eb5445bc7eb54678170a1ab74d04ad7b9e6c9adf
AlviGeo <alvigeovan29@gmail.com>
2026-01-24 02:37:25 +0700

    feat(api): add crud integration using panache

--- src/main/java/com/cofecode/services/GameService.java
+++ src/main/java/com/cofecode/services/GameService.java
@@ -1,15 +1,68 @@
 package com.cofecode.services;
 
+import com.cofecode.entity.Games;
+import com.cofecode.repository.GamesRepository;
 import jakarta.enterprise.context.Dependent;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Dependent
 public class GameService {
 
-    @ConfigProperty(name="test")
-    String test;
+    @Inject
+    private GamesRepository gamesRepository;
 
-    public void test() {
-        System.out.println(test);
+    public List<Games> getAllGames() {
+        return gamesRepository.listAll();
+    }
+
+    public List<Games> findPaginated(int page, int pageSize, String sortField, String name) {
+        if (name != null && !name.isEmpty()) {
+            return gamesRepository.findPaginatedByName(page, pageSize, sortField, name);
+        }
+        return gamesRepository.findPaginated(page, pageSize, sortField);
+    }
+
+    public long count(String name) {
+        if (name != null && !name.isEmpty()) {
+            return gamesRepository.countByName(name);
+        }
+        return gamesRepository.count();
+    }
+
+    public Optional<Games> findById(long id) {
+        return gamesRepository.findByIdOptional(id);
+    }
+
+    @Transactional
+    public void createGames(Games game) {
+        game.setId(0);
+        gamesRepository.persist(game);
+    }
+
+    @Transactional
+    public void replaceGame(Games game) {
+        gamesRepository.findByIdOptional(game.getId()).ifPresent(v -> gamesRepository.persist(game));
+    }
+
+    public void updateGame(long id, String name, String category) {
+        gamesRepository.findByIdOptional(id).ifPresent(v -> {
+            if (name != null && !name.isEmpty()) {
+                v.setName(name);
+            }
+            if (category != null && !category.isEmpty()) {
+                v.setCategory(category);
+            }
+            gamesRepository.persist(v);
+        });
+    }
+
+    public void deleteGame(long id) {
+        gamesRepository.findByIdOptional(id).ifPresent(v -> {
+            gamesRepository.delete(v);
+        });
     }
 }
