74fbbc8647a2144a3cf3cadc312fc3d42effb8ad
AlviGeo <alvigeovan29@gmail.com>
2026-01-26 14:38:53 +0700

    test: add mockito JUnit jupiter dependency

--- src/test/java/com/cofecode/GreetingResourceTest.java
+++ src/test/java/com/cofecode/GreetingResourceTest.java
@@ -1,18 +1,23 @@
 package com.cofecode;
 
+
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
-class GreetingResourceTest {
+public class GameResourceTest {
+
     @Test
-    public void testGetGames() {
-        given()
-                .when().get("/games")
+    public void testGetUsersEndpoint() {
+        RestAssured.given()
+                .when().get("/games/1")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("id", equalTo(1))
+                .body("name", equalTo("Tomb Rider"))
+                .body("category", equalTo("Action"));
     }
 }
